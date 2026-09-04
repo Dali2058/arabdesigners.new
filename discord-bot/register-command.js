56af22dcbbd15a641bdc1ac16c33e7acc49de3a9
Dali2058 <167540250+Dali2058@users.noreply.github.com>
2026-09-04 18:58:55 +0300

    Delete discord-bot directory

--- discord-bot/register-command.js
+++
@@ -1,5 +0,0 @@
-import { REST, Routes, SlashCommandBuilder } from 'discord.js';
-const command=new SlashCommandBuilder().setName('login').setDescription('Open Arab Designers Discord login');
-const rest=new REST({version:'10'}).setToken(process.env.DISCORD_BOT_TOKEN);
-await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),{body:[command.toJSON()]});
-console.log('Registered /login');
